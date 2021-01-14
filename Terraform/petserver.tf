d65e985bf4a9b52dd6226cf8f1f5cae3a24c8621
unknown <joelle.saurer@devoteam.com>
2021-01-14 23:03:04 +0100

    terra

--- Terraform/petserver.tf
+++ Terraform/petserver.tf
@@ -97,3 +97,25 @@ resource "azurerm_network_interface_security_group_association" "nisg" {
     network_interface_id      = azurerm_network_interface.terraformnic.id
     network_security_group_id = azurerm_network_security_group.terraformnsg.id
 }
+
+#Create storage account
+resource "random_id" "randomId" {
+    keepers = {
+        # Generate a new ID only when a new resource group is defined
+        resource_group = azurerm_resource_group.rg.name
+    }
+
+    byte_length = 8
+}
+
+resource "azurerm_storage_account" "storageaccount" {
+    name                        = "diag${random_id.randomId.hex}"
+    resource_group_name         = azurerm_resource_group.rg.name
+    location                    = "westeurope"
+    account_replication_type    = "LRS"
+    account_tier                = "Standard"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
