e1276b8e2a0c6fa71abb00fb83ab83e0f923b57b
unknown <joelle.saurer@devoteam.com>
2021-01-14 21:36:35 +0100

    terra

--- Terraform/petserver.tf
+++ Terraform/petserver.tf
@@ -1,20 +1,17 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 2.70"
+    azurerm = {
+      source  = "hashicorp/azurerm"
     }
   }
 }
 
-provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-  access_key = "AKIAJTWHUZ5W4P6PSNOA"
-  secret_key = "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"
+provider "azurerm" {
+  features {}
 }
 
-resource "aws_instance" "localhost" {
-  ami           = "ami-0885b1f6bd170450c"
-  instance_type = "t2.micro"
+
+resource "azurerm_resource_group" "rg" {
+  name     = "petclinic"
+  location = "westeurope"
 }
