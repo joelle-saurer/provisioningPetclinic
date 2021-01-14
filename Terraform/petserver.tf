b41a088c0e8f0009e73ac645bfeaae7990fea7f7
unknown <joelle.saurer@devoteam.com>
2021-01-14 22:07:58 +0100

    tf

--- Terraform/petserver.tf
+++ Terraform/petserver.tf
@@ -10,11 +10,43 @@ provider "azurerm" {
   features {}
 }
 
-
+#Create resource group
 resource "azurerm_resource_group" "rg" {
   name     = "petclinic"
   location = "westeurope"
   tags = {
       environment = "Terraform Demo"
   }
+}
+
+#Create virtual network
+resource "azurerm_virtual_network" "terraformnetwork" {
+    name                = "myVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "westeurope"
+    resource_group_name = azurerm_resource_group.rg.name
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+#Create subnet 
+resource "azurerm_subnet" "terraformsubnet" {
+    name                 = "mySubnet"
+    resource_group_name  = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.terraformnetwork.name
+    address_prefixes       = ["10.0.2.0/24"]
+}
+
+#Create public IP address
+resource "azurerm_public_ip" "terraformpublicip" {
+    name                         = "myPublicIP"
+    location                     = "westeurope"
+    resource_group_name          = azurerm_resource_group.rg.name
+    allocation_method            = "Dynamic"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
 }
