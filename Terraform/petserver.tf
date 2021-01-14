f87bfa305c64b3b82f0b8ace2a52757860882ca6
unknown <joelle.saurer@devoteam.com>
2021-01-14 18:04:27 +0100

    group

--- Terraform/petserver.tf
+++ Terraform/petserver.tf
@@ -1,20 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 2.70"
-    }
-  }
+# Configure the Azure Provider
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.40.0"
+  features {}
 }
 
-provider "aws" {
-  profile = "default"
-  region  = "us-west-1"
-  access_key = "AKIAIAZQETEYOFOF6EEQ"
-  secret_key = "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"
-}
-
-resource "aws_instance" "localhost" {
-  ami           = "ami-00831fc7c1e3ddc60"
-  instance_type = "t2.micro"
+# Create a resource group
+resource "azurerm_resource_group" "petclinic" {
+  name     = "devopsgroup"
+  location = "West Europe"
 }
