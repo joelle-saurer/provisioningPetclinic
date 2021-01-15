b1f0b25f3d3ee75ee6dd37a921f45b79a242337c
unknown <joelle.saurer@devoteam.com>
2021-01-15 13:09:05 +0100

    terra

--- Terraform/petserver.tf
+++ Terraform/petserver.tf
@@ -44,7 +44,7 @@ resource "azurerm_public_ip" "terraformpublicip" {
     name                         = "myPublicIP"
     location                     = "westeurope"
     resource_group_name          = azurerm_resource_group.rg.name
-    allocation_method            = "Dynamic"
+    allocation_method            = "Static"
 
     tags = {
         environment = "Terraform Demo"
